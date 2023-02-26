dc22a63c045731b551f623aae1b7ac8439699c73
KernelByte <yonihermelendez@gmail.com>
2023-02-26 15:20:53 -0500

    Se añade clase sobre la sentencia switch

---
+++ switch.js
@@ -0,0 +1,14 @@
+
+var numero = 1;
+
+switch (numero) {
+    case 1:
+        console.log("El valor es uno");
+        break;
+    case 2:
+        console.log("El valor es dos");
+         break;
+    default:
+        console.log("El valor no es uno");
+        break;
+}
