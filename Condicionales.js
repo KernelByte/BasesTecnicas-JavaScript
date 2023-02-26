8e22739f2f57bbfc8666bca751a3e4b532245f47
KernelByte <yonihermelendez@gmail.com>
2023-02-26 15:15:30 -0500

    se añade clase sobre los condicionales

---
+++ Condicionales.js
@@ -0,0 +1,18 @@
+var edad = 20;
+
+if (edad > 18){
+    console.log("Es mayor de edad")
+}
+else if(edad < 18 ){
+    console.log("No es mayor de edad")
+} 
+else {
+    console.log("Apenas cumplio los 18 años de edad")
+}
+
+// Operador ternario
+condicion ? true : false
+
+var numero = 1;
+
+var resultado = numero === 1 ? "Si es uno" : "No es uno";
