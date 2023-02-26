df5b223f42a7849f8215d54acae72871970f73a8
KernelByte <yonihermelendez@gmail.com>
2023-02-26 15:04:02 -0500

    Se añade clase sobre los diferentes tipos de operadores

---
+++ Operadores.js
@@ -0,0 +1,31 @@
+
+// Operadores Matematicos
+a + b
+a - b
+a * b
+a / b
+
+// Demas operadores
+"Yoniher " + "Melendez" // Operador de concatenacion
+
+!false     // Operador NOT
+
+var a = 1; // Operador de asignacion
+
+3 == "3" // Operador de comparacion
+
+3 === "3" // Operador de comparacion con tipo de valor tambien
+
+5 > 3 // Operador mayor
+
+5 < 3 // Operador menor
+
+5 <= 6 // Operador menor o igual
+
+5 >= 6 // Operador mayor o igual
+
+a && b // Operador AND
+
+a || b // Operador OR
+
+41++  // Operador de incremento
