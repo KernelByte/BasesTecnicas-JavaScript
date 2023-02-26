4a8bcc1a09209d0a6296d3c57c63104a5cf0733c
KernelByte <yonihermelendez@gmail.com>
2023-02-26 18:13:04 -0500

    Se añade clase sobre los array

---
+++ Arrays.js
@@ -0,0 +1,12 @@
+var datos = ["oscar","LuisAa","Marta","Luis"]
+console.log(datos[2])
+
+// longitud del arreglo
+console.log(datos.length)
+
+// Metodos del arreglo
+datos.push("Julian") // Añadir elementos al arreglo en la ultima posicion
+datos.unshift("Marcela"); // Añade elementos al arreglo pero en la primera posicion
+datos.pop() // elimina el ultimo elemento en el arreglo
+datos.shift() // elimina el primer elemento del arreglo
+datos.indexOf("Marta") // saber el index o la posicion de un elemento en el arreglo
