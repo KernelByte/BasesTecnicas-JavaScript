a8514387b412f321419cbb04676c104c03b917bf
KernelByte <yonihermelendez@gmail.com>
2023-02-26 13:13:21 -0500

    Se añade clase sobre la declaracion de variables

---
+++ Variables.js
@@ -0,0 +1,16 @@
+
+// Declaracion de variables
+var nombreVariable = "Oscar Lopez";
+
+//Declaracion
+var edad;
+//Inicializacion
+edad = 10;
+
+//Variable de elementos
+var elementos = [1,2,3,4,5]
+
+var objeto = {
+    nombre : "Yoniher",
+    edad : 27
+}
