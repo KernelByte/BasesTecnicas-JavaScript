44632881950751d4021a770f01da4bb8f9792954
KernelByte <yonihermelendez@gmail.com>
2023-02-26 15:01:00 -0500

    Se da clase sobre las funciones y sus declaraciones.

---
+++ Funciones.js
@@ -0,0 +1,20 @@
+// Declaracion de funciones
+
+// Funcion declarativa
+function sumaNumeros(a,b){
+    return a + b;
+}
+
+
+// Funcion expresiva
+var totalSuma = function(a , b){
+    return a + b;
+}
+
+// Impresion de funcion
+function saludar(nombre) {
+	console.log(`Hola ${nombre}`);
+}
+
+// Llamar a la funcion
+saludar('Diego');
