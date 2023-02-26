7f22acd0bf413796445b2e93068e2e603b2dc636
KernelByte <yonihermelendez@gmail.com>
2023-02-26 12:53:03 -0500

    Se añade clase sobre los tipos de valores y como verificar el tipo de cada uno

---
+++ Valores.js
@@ -0,0 +1,25 @@
+/** Valores primitivos */
+
+// Valores numericos
+10
+
+// Valores de texto
+"Cadena de texto"
+
+// Valores booleanos
+true
+false
+
+// Valores Nulos o no definidos
+null
+undefined
+
+
+/** Valores no primitivos */
+
+//Valores de arreglo
+[1,2,3]
+// Tipo objeto
+{ nombre : "Oscar lopez"}
+
+// Palabra reservada para saber el tipo de una variable es: typeof
