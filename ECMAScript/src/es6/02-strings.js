898f24cebd253d50cf5c24925399a1926fabc67f
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-02-28 14:21:47 -0500

    Se añade clase de string para concatenar variables o espacios de linea

---
+++ ECMAScript/src/es6/02-strings.js
@@ -0,0 +1,21 @@
+//let hello = "Hello";
+//let wolrd = "Wolrd";
+
+let concat = hello + " " + wolrd + "!";
+console.log(concat);
+
+ // Template literals
+ let hello = "Hello";
+ let wolrd = "Wolrd";
+ let concat2 = `${hello} ${wolrd}!`;
+ console.log(concat2);
+
+ // Multi-line String
+ let lorem = "Esto es un string \n" + "esto es otra linea";
+ console.log(lorem);
+
+ let lorem2 = `Esto es una linea
+ y esto es otra linea
+ `;
+ console.log(lorem2);
+ 
