18b74e16d4c62e5663034dfd5fc308bc0af59f01
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-02-28 16:46:41 -0500

    Se añade clase sobre object literals

---
+++ ECMAScript/src/es6/05-object-literals.js
@@ -0,0 +1,17 @@
+ // Manera antigua 
+ function objeto (name, age, country){
+    return {
+        name : name, 
+        age : age, 
+        country : country
+    }
+ };
+
+ // Forma abreviada
+ function objeto (name, age, country){
+    return {
+        name,
+        age,  
+        country 
+    }
+ };
