6f8050caa73020509406341ab025b5e9d230ce24
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-02-28 15:13:05 -0500

    Se añade clase de restruturacion en objetos y funciones de manera dinamica

--- ECMAScript/src/es6/04-rest-spread.js
+++ ECMAScript/src/es6/04-rest-spread.js
@@ -9,3 +9,18 @@ console.log(a,fruits[1]);
 let user = {name : "Oscar", age : 18, country : "Colombia"};
 let {name, age, country} = user;
 console.log(name,age,user.country);
+
+// Spread operator
+let person = {name : "Maria", age : 15};
+let size = 1.76;
+
+// restructuracion del objeto
+let data = {...person, size};
+console.log(data);
+
+// rest - restructuracion de funciones
+function sum(num, ...values){
+    console.log(values);
+}
+
+sum(2,3,4,6);
