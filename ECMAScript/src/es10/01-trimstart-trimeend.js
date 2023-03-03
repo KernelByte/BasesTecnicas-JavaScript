85280d5f35c03cf96363cee477ad92684f5a734a
Yoniher Melendez <kernelbyte_developer@hotmail.com>
2023-03-03 07:25:46 -0500

    Se añade clase sobre el metodo Trim

---
+++ ECMAScript/src/es10/01-trimstart-trimeend.js
@@ -0,0 +1,6 @@
+
+// Metodo Trim para eliminar caracteres dentro de un string
+const hello = "      Hello World!    ";
+//console.log(hello.trimStart());
+//console.log(hello.trimEnd());
+console.log(hello.trim());
